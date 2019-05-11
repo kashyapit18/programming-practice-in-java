0a8842bd68e2e2734f14a2e48893c51ac5444c80
kashyapit18 <ujha246@gmail.com>
2019-05-11 23:09:30 +0530

    Add files via upload

---
+++ LargestElement.java
@@ -0,0 +1,16 @@
+public class LargestElement {
+	public static void main(String[] args){
+		int[] array = {4,1,2,3,7,0,5,6,9,8};
+		System.out.println("The largest of the elements of the array is " + findLargest(array));
+	}
+	public static int findLargest(int[] array){
+		int max = 0;
+		for(int i = 1; i < array.length; i++){
+			if(array[i] > max){
+				max = array[i];
+			}
+		}
+		return max;
+	}
+
+}
